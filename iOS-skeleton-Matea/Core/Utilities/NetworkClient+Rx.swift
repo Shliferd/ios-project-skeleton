c146d13f182b9bd7996a763e4accaddf45d44aa8
Andrei Matea <andrei.matea@halcyonmobile.com>
2020-08-16 21:27:38 +0300

    add rx for restBird networkClient

--- iOS-skeleton-Matea/Core/Utilities/NetworkClient+Rx.swift
+++ iOS-skeleton-Matea/Core/Utilities/NetworkClient+Rx.swift
@@ -7,3 +7,61 @@
 //
 
 import RxSwift
+import RestBird
+
+extension NetworkClient: ReactiveCompatible {}
+
+extension Reactive where Base: NetworkClient {
+    
+    // MARK: - Data request
+    
+    public func execute<Request: DataRequest>(request: Request) -> Single<Void> where Request.ResponseType == EmptyResponse {
+        return Single.create { [base] event -> Disposable in
+            base.execute(request: request) { (result: Result<Void, Error>) in
+                switch result {
+                case .success: event(.success(()))
+                case .failure(let error): event(.error(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    public func execute<Request: DataRequest>(request: Request) -> Single<Request.ResponseType> {
+        return Single.create { [base] event -> Disposable in
+            base.execute(request: request) { (result: Result<Request.ResponseType, Error>) in
+                switch result {
+                case .success(let result): event(.success(result))
+                case .failure(let error): event(.error(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    // MARK: - Multipart
+    
+    public func execute<Request: MultipartRequest>(request: Request) -> Single<Void> where Request.ResponseType == EmptyResponse {
+        return Single.create { [base] event -> Disposable in
+            base.execute(request: request, uploadProgress: nil) { (result: Result<Request.ResponseType, Error>) in
+                switch result {
+                case .success: event(.success(()))
+                case .failure(let error): event(.error(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    public func execute<Request: MultipartRequest>(request: Request) -> Single<Request.ResponseType> {
+        return Single.create { [base] event -> Disposable in
+            base.execute(request: request, uploadProgress: nil) { (result: Result<Request.ResponseType, Error>) in
+                switch result {
+                case .success(let result): event(.success(result))
+                case .failure(let error): event(.error(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+}
